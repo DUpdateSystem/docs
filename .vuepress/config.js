1697462e43c443847f38e645d66f7c07a3e7abfa
MoonBegonia <i@moonbegonia.xyz>
2020-08-04 20:06:50 +0800

    fix: 设置语言为中文无效

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  lang: 'zh-CN',
   title: 'Distributed Update System',
   description: 'Upgrade anything any where',
   head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
   themeConfig: {
     nav: [
       { text: '使用指南', link: '/guide/' },
@@ -13,33 +17,40 @@ module.exports = {
         items: [
           {
             text: 'Android 端（酷安）',
-            link: 'https://www.coolapk.com/apk/net.xzos.upgradeall'
+            link: 'https://www.coolapk.com/apk/net.xzos.upgradeall',
           },
           {
             text: 'Android 端（GitHub）',
-            link: 'https://github.com/DUpdateSystem/UpgradeAll/releases'
-          }
-        ]
+            link: 'https://github.com/DUpdateSystem/UpgradeAll/releases',
+          },
+        ],
       },
-      { text: 'GitHub', link: 'https://github.com/DUpdateSystem' }
+      { text: 'GitHub', link: 'https://github.com/DUpdateSystem' },
     ],
     sidebarDepth: 2,
     sidebar: [
       {
         title: '使用指南',
-        path: '/guide/'
+        path: '/guide/',
       },
       {
         title: 'FAQ',
-        path: '/faq/'
+        path: '/faq/',
       },
       {
         title: '参与我们',
         path: '/joinus/',
-        children: ['/joinus/','/joinus/contributing', '/joinus/docs', 'joinus/app-rules' , 'joinus/python-scripts', 'joinus/bs4-function']
-      }
+        children: [
+          '/joinus/',
+          '/joinus/contributing',
+          '/joinus/docs',
+          'joinus/app-rules',
+          'joinus/python-scripts',
+          'joinus/bs4-function',
+        ],
+      },
     ],
-    lastUpdated: '最后更新时间'
+    lastUpdated: '最后更新时间',
   },
-  plugins: [['@vuepress/active-header-links'], ['@vuepress/back-to-top']]
+  plugins: [['@vuepress/active-header-links'], ['@vuepress/back-to-top']],
 };
